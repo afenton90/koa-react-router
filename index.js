b35a9f302fd9be5cbf43e1d767173338b640d09e
Alex Fenton <afenton90@gmail.com>
2019-04-29 11:36:14 +0100

    chore(deps): upgrades to latest version of dev deps

--- index.js
+++ index.js
@@ -8,61 +8,60 @@ const server = ({
   onRedirect,
   onRender,
   preRender = router => router
-}) =>
-  async (ctx, next) => {
-    try {
-      const location = ctx.request.url;
-      const routerContext = {};
+}) => async (ctx, next) => {
+  try {
+    const location = ctx.request.url;
+    const routerContext = {};
 
-      const router = (
-        <StaticRouter
-          location={location}
-          context={routerContext}
-        >
-          <App />
-        </StaticRouter>
-      );
+    const router = (
+      <StaticRouter
+        location={location}
+        context={routerContext}
+      >
+        <App />
+      </StaticRouter>
+    );
 
-      const updatedComponent = await preRender(router);
+    const updatedComponent = await preRender(router);
 
-      if (!updatedComponent) throw new Error('No component returned from preRender');
+    if (!updatedComponent) throw new Error('No component returned from preRender');
 
-      const view = renderToString(updatedComponent);
+    const view = renderToString(updatedComponent);
 
-      ctx.state = {
-        ...ctx.state,
-        routerContext
-      };
+    ctx.state = {
+      ...ctx.state,
+      routerContext
+    };
 
-      if (routerContext.url) await onRedirect(ctx, routerContext.url);
-      else {
-        const {
-          Container,
-          containerRenderer
-        } = await onRender(ctx);
+    if (routerContext.url) await onRedirect(ctx, routerContext.url);
+    else {
+      const {
+        Container,
+        containerRenderer
+      } = await onRender(ctx);
 
-        let rendered;
-        if (containerRenderer) {
-          rendered = renderToStaticMarkup(containerRenderer(view));
-        } else {
-          rendered = renderToStaticMarkup(
-            <Container>
-              <div dangerouslySetInnerHTML={{ __html: view }} />
-            </Container>
-          );
-        }
-
-        if (routerContext.status) ctx.response.status = routerContext.status;
-        ctx.response.body = `
-          <!doctype html>
-          ${rendered}
-        `;
+      let rendered;
+      if (containerRenderer) {
+        rendered = renderToStaticMarkup(containerRenderer(view));
+      } else {
+        rendered = renderToStaticMarkup(
+          <Container>
+            <div dangerouslySetInnerHTML={{ __html: view }} />
+          </Container>
+        );
       }
-    } catch (err) {
-      await onError(ctx, err);
-    } finally {
-      await next();
+
+      if (routerContext.status) ctx.response.status = routerContext.status;
+      ctx.response.body = `
+        <!doctype html>
+        ${rendered}
+      `;
     }
-  };
+  } catch (err) {
+    await onError(ctx, err);
+  } finally {
+    await next();
+  }
+};
 
 export default server;
